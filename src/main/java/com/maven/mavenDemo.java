53f0246b2aafbdebb67b98c2d9a3b0ec2d030dd2
Oly <1592496726@qq.com>
2021-04-29 23:17:57 +0800

    第一次提交src

---
+++ src/main/java/com/maven/mavenDemo.java
@@ -0,0 +1,13 @@
+package com.maven;
+
+/**
+ * @Auther: OuJie
+ * @Date: 2021/4/29 - 04 - 29 - 23:04
+ * @Description: com.maven
+ * @version: 1.0
+ */
+public class mavenDemo {
+    public static void main(String[] args) {
+        int a=10;
+    }
+}
